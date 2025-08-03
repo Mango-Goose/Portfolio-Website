e0b5e15e5e090f491c36121e6d09e17594bc50f8
Mango-Goose <113191075+Mango-Goose@users.noreply.github.com>
2025-08-03 22:19:40 +0100

    Changed top padding to be more central on the homepage

--- portfolio-website/resources/views/home.blade.php
+++ portfolio-website/resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <body class="">
     <div class="bg-img bg-cover bg-center h-screen flex flex-col items-center justify-center">
-    <h1 class="text-9xl pt-[200px] pb-[80px] text-rose-100">Naomi Pollinger</h1>
+    <h1 class="text-9xl pt-[80px] pb-[80px] text-rose-100">Naomi Pollinger</h1>
      <h1 class="text-5xl text-rose-100">Welcome to my portfolio!</h1>
     </div>
     </body>
