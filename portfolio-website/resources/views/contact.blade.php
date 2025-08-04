0b9b5799802b2d9ebd6acb44e787e82681e14c1a
Mango-Goose <113191075+Mango-Goose@users.noreply.github.com>
2025-08-04 12:29:00 +0100

    Small design updates to the contact form
    Made the textarea align with the rest of the input fields and also changed the colour of the footer to be more cohesive with the form.

--- portfolio-website/resources/views/contact.blade.php
+++ portfolio-website/resources/views/contact.blade.php
@@ -5,15 +5,15 @@
     <div class="border-2 rounded-xl ml-80 mr-80 text-rose-100 flex flex-col items-center py-5">
         <h2 class="py-4 text-4xl">Contact Form</h2>
         <p class="pb-10 text-l opacity-75">You can use this form for any inquiries about me and my work!</p>
-        <form class="flex-col flex ">
+        <form class="flex-col flex grid-flow-col">
             <div class="flex justify-evenly">
-                <input type="text" id="fName" name="fName" placeholder="First Name" class="m-4 bg-zinc-800 p-3 rounded-lg placeholder-zinc-500">
-                <input type="text" id="lName" name="lName" placeholder="Last Name" class="m-4 bg-zinc-800 p-3 rounded-lg placeholder-zinc-500">
+                <input type="text" id="fName" name="fName" placeholder="First Name" class="m-4 bg-zinc-800 p-3 rounded-lg placeholder-zinc-400">
+                <input type="text" id="lName" name="lName" placeholder="Last Name" class="m-4 bg-zinc-800 p-3 rounded-lg placeholder-zinc-400">
             </div>
-            <input type="text" id="email" name="email" placeholder="Email" class="m-4 bg-zinc-800 p-3 rounded-lg placeholder-zinc-500">
-            <input type="text" id="inquiryType" name="inquiryType" placeholder="Inquiry" class="m-4 bg-zinc-800 p-3 rounded-lg placeholder-zinc-500">
-            <input type="textarea" id="fName" name="fName" placeholder="Add more details here!"class=" h-48 m-8 bg-zinc-800 p-3 rounded-lg placeholder-zinc-500 placeholder"></textarea>
-            <input type="submit" Value="Submit" class="hover:opacity-75 text-2xl bg-zinc-900  rounded-xl px-7 py-3 mb-5">
+            <input type="text" id="email" name="email" placeholder="Email" class="m-4 bg-zinc-800 p-3 rounded-lg placeholder-zinc-400">
+            <input type="text" id="inquiryType" name="inquiryType" placeholder="Inquiry" class="m-4 bg-zinc-800 p-3 rounded-lg placeholder-zinc-400">
+            <textarea id="description" name="description" rows="5" placeholder="Add more details here!"class=" resize:none m-4 mb-8 bg-zinc-800 p-3 rounded-lg placeholder-zinc-400 placeholder"></textarea>
+            <input type="submit" Value="Submit" class="hover:opacity-75 text-2xl bg-zinc-800  rounded-xl py-3 mb-5">
         </form>
     </div>
 </x-layout>
