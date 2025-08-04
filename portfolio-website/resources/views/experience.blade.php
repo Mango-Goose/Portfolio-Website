d21ff708d78b48645121dc3ad864c6a5129af52e
Mango-Goose <113191075+Mango-Goose@users.noreply.github.com>
2025-08-04 15:47:01 +0100

    Improved formatting of the CV

--- portfolio-website/resources/views/experience.blade.php
+++ portfolio-website/resources/views/experience.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-    <div class="text-rose-100 flex items-center flex-col">
+    <div class="text-rose-100 flex items-center flex-col ml-28 mr-28 pb-16 border-2 border-rose-100 rounded-2xl">
     <h2 class="text-bold text-6xl py-16 ">My CV</h2>
-    <img src="/images/CV_Naomi_Pollinger.png" class="w-3/4 h-fit"></img>
+    <img src="/images/CV_Naomi_Pollinger.png" class="w-2/3 h-fit rounded-2xl"></img>
 </div>
 </x-layout>
